7b99cac3c72fd0d6a95db705b099fcdc7ce1196a
Yecheng Fu <cofyc.jackson@gmail.com>
2010-03-20 21:35:23 +0800

    step 2

--- ct_display.c
+++ ct_display.c
@@ -19,8 +19,6 @@
 #define XCOLOR_OF(cell)     ((cell) & 0x0F)
 #define XSTATUS_OF(cell)    ((cell) >> 7)
 
-WINDOW *win_default, *win_screen, *win_sidebar;
-
 static int score = 0;
 
 struct ct_window {
@@ -29,8 +27,8 @@ struct ct_window {
     int y;
 };
 
-struct ct_window ct_win_sidebar;
 struct ct_window ct_win_main;
+struct ct_window ct_win_sidebar;
 
 static char ct_screen_buffer[CT_SCREEN_Y][CT_SCREEN_X];
 static char ct_screen_bg[CT_SCREEN_Y][CT_SCREEN_X];
@@ -41,15 +39,22 @@ static void ct_display_update(int top_y, int btm_y, int lft_x, int rgt_x);
 int
 ct_display_init()
 {
-    /* default window called strscr */
-    win_default = initscr();    /* initialize the curses library, return pointer to stdscr */
-    cbreak();                   /* take input chars one at a time, no wait for \n */
+    /* initialize the curses library, return pointer to stdscr */
+    initscr();
+    /* take input chars one at a time, no wait for \n */
+    cbreak();
     noecho();
-    nodelay(win_default, false);        /* if false, then reads will block */
-    nonl();                     /* tell curses not to do NL->CR/NL on output */
-    keypad(win_default, true);  /* enable keyboard mapping (function key -> single value) */
-    // mac os x terminal don't support this currently
-    curs_set(0);                /* set cursor invisible */
+    /* if false, then reads will block */
+    nodelay(stdscr, false);
+    /* tell curses not to do NL->CR/NL on output */
+    nonl();
+    /* enable keyboard mapping (function key -> single value) */
+    keypad(stdscr, true);
+    /*
+     * set cursor invisible
+     * @Note: mac os x terminal don't support this currently
+     */
+    curs_set(0);
 
     if (has_colors()) {
         start_color();
@@ -67,42 +72,37 @@ ct_display_init()
         init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
         init_pair(7, COLOR_WHITE, COLOR_BLACK);
     }
-
-    win_screen = newwin(CT_SCREEN_Y * CT_SCREEN_CELL_Y, CT_SCREEN_X * CT_SCREEN_CELL_X, 0, 0);
-
-    // borders
-    int i, j;
-    wattrset(win_default, COLOR_PAIR(7 % 8));
-    for (i = CT_SCREEN_Y; i < CT_SCREEN_Y + 1; i++) {
-        for (j = 0; j < CT_SCREEN_X + 1; j++) {
-            mvwaddch(win_default, i * CT_SCREEN_CELL_Y, j * CT_SCREEN_CELL_X, '[');
-            mvwaddch(win_default, i * CT_SCREEN_CELL_Y, j * CT_SCREEN_CELL_X + 1, ']');
-        }
-    }
-    for (i = 0; i < CT_SCREEN_Y + 1; i++) {
-        for (j = CT_SCREEN_X; j < CT_SCREEN_X + 1; j++) {
-            mvwaddch(win_default, i * CT_SCREEN_CELL_Y, j * CT_SCREEN_CELL_X, '[');
-            mvwaddch(win_default, i * CT_SCREEN_CELL_Y, j * CT_SCREEN_CELL_X + 1, ']');
-        }
-    }
-    wrefresh(win_default);
-
     // bg
-    for (i = 0; i < CT_SCREEN_Y; i++) {
-        for (j = 0; j < CT_SCREEN_X; j++) {
+    for (int i = 0; i < CT_SCREEN_Y; i++) {
+        for (int j = 0; j < CT_SCREEN_X; j++) {
             ct_screen_buffer[i][j] = 0;
         }
     }
 
-    // sidebar info
-    win_sidebar =
-        newwin(CT_SIDEBAR_Y * CT_SCREEN_CELL_Y, CT_SIDEBAR_X * CT_SCREEN_CELL_X, 0,
-               (CT_SCREEN_X + 1) * CT_SCREEN_CELL_X);
+    // bg borders
+    wattrset(stdscr, COLOR_PAIR(7 % 8));
+    for (int i = CT_SCREEN_Y; i < CT_SCREEN_Y + 1; i++) {
+        for (int j = 0; j < CT_SCREEN_X + 1; j++) {
+            mvwaddch(stdscr, i * CT_SCREEN_CELL_Y, j * CT_SCREEN_CELL_X, '[');
+            mvwaddch(stdscr, i * CT_SCREEN_CELL_Y, j * CT_SCREEN_CELL_X + 1, ']');
+        }
+    }
+    for (int i = 0; i < CT_SCREEN_Y + 1; i++) {
+        for (int j = CT_SCREEN_X; j < CT_SCREEN_X + 1; j++) {
+            mvwaddch(stdscr, i * CT_SCREEN_CELL_Y, j * CT_SCREEN_CELL_X, '[');
+            mvwaddch(stdscr, i * CT_SCREEN_CELL_Y, j * CT_SCREEN_CELL_X + 1, ']');
+        }
+    }
+    wrefresh(stdscr);
 
-    ct_win_main.win = win_default;
+    // sidebar info
+    ct_win_main.win = newwin(CT_SCREEN_Y * CT_SCREEN_CELL_Y, CT_SCREEN_X * CT_SCREEN_CELL_X, 0, 0);
     ct_win_main.x = CT_SCREEN_X;
     ct_win_main.y = CT_SCREEN_Y;
-    ct_win_sidebar.win = win_sidebar;
+
+    ct_win_sidebar.win =
+        newwin(CT_SIDEBAR_Y * CT_SCREEN_CELL_Y, CT_SIDEBAR_X * CT_SCREEN_CELL_X, 0,
+               (CT_SCREEN_X + 1) * CT_SCREEN_CELL_X);
     ct_win_sidebar.x = CT_SIDEBAR_X;
     ct_win_sidebar.y = CT_SIDEBAR_Y;
 
@@ -120,19 +120,20 @@ ct_display_update_sidebar()
     if (b) {
         for (i = 0; i < 4; i++) {
             for (j = 0; j < 4; j++) {
-                ct_display_show_cell(&ct_win_sidebar, i + 1, j + 1,
-                                     b->show[i][j]);
+                ct_display_show_cell(&ct_win_sidebar, i + 1, j + 1, b->show[i][j]);
             }
 
         }
     }
-    // 
-    mvwprintw(win_sidebar, 6, 2, "score: %d", score);
-    mvwprintw(win_sidebar, 7, 2, "change: k");
-    mvwprintw(win_sidebar, 8, 2, "left: h right: l");
-    mvwprintw(win_sidebar, 9, 2, "down: j");
-    mvwprintw(win_sidebar, 10, 2, "fast down: [space]");
-    wrefresh(win_sidebar);
+    // info
+    mvwprintw(ct_win_sidebar.win, 6, 2, "score: %d", score);
+    mvwprintw(ct_win_sidebar.win, 7, 2, "change: k");
+    mvwprintw(ct_win_sidebar.win, 8, 2, "left: h right: l");
+    mvwprintw(ct_win_sidebar.win, 9, 2, "down: j");
+    mvwprintw(ct_win_sidebar.win, 10, 2, "fast down: [space]");
+
+    // refresh
+    wrefresh(ct_win_sidebar.win);
 }
 
 static void
@@ -144,7 +145,6 @@ ct_display_show_cell(struct ct_window *ct_win, int y, int x, char cell)
     } else if (x < 0 || x >= ct_win->x) {
         ct_debug_log("x reach out of screen");
     }
-
     // color
     wattrset(ct_win->win, COLOR_PAIR(XCOLOR_OF(cell) % 8));
 
@@ -184,12 +184,11 @@ ct_display_update(int top_y, int btm_y, int lft_x, int rgt_x)
     // show
     for (i = top_y; i <= btm_y; i++) {
         for (j = lft_x; j <= rgt_x; j++) {
-            ct_display_show_cell(&ct_win_main, i, j,
-                                 ct_screen_buffer[i][j]);
+            ct_display_show_cell(&ct_win_main, i, j, ct_screen_buffer[i][j]);
         }
     }
 
-    wrefresh(win_screen);
+    wrefresh(ct_win_main.win);
 }
 
 int
